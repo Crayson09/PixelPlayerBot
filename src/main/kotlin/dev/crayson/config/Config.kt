c2e5ecab1513a877e28a13afecf41c6e71a82bf9
Ole H <olehadeler@hotmail.com>
2025-08-19 22:52:41 +0200

    update .gitignore

--- src/main/kotlin/dev/crayson/config/Config.kt
+++ src/main/kotlin/dev/crayson/config/Config.kt
@@ -3,6 +3,6 @@ package dev.crayson.config
 @JvmRecord
 data class Config(
     val token: String = "TOKEN",
-    val welcomeChannelId: Long,
-    val joinRoles: List<Long> = listOf()
+    val welcomeChannelId: Long = 1,
+    val joinRoles: List<Long> = listOf(1),
 )
